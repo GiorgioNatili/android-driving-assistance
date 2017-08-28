488340e51b22c5f247d2a5d5b84a73ca4b940ec5
Giorgio <g.natili@gnstudio.com>
2017-08-28 08:07:03 -0400

    Fix OpenCV integration

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,8 @@
 #include <jni.h>
 #include <string>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
 
 extern "C"
 JNIEXPORT jstring JNICALL

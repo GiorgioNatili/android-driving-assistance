e841972bf0dcb850c2228771fdf49a495831ef91
Giorgio <g.natili@gnstudio.com>
2017-08-31 10:22:30 -0400

    Fix binary images parsing

--- app/src/main/java/io/a2xe/experiments/myapplicationc/OpenCVCamera.kt
+++ app/src/main/java/io/a2xe/experiments/myapplicationc/OpenCVCamera.kt
@@ -5,12 +5,12 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.util.Log
 import android.view.*
+import org.opencv.android.*
 
-import org.opencv.android.BaseLoaderCallback
-import org.opencv.android.CameraBridgeViewBase
-import org.opencv.android.LoaderCallbackInterface
-import org.opencv.android.OpenCVLoader
+import org.opencv.core.Core
 import org.opencv.core.Mat
+import org.opencv.core.Scalar
+import org.opencv.core.Size
 import org.opencv.imgproc.Imgproc
 
 class OpenCVCamera : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListener2 {
@@ -21,6 +21,7 @@ class OpenCVCamera : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
     private lateinit var itemPreviewGray: MenuItem
     private lateinit var previewCanny: MenuItem
     private lateinit var previewFeatures: MenuItem
+    private lateinit var previewBinary: MenuItem
 
     private var viewMode: Int = Int.MAX_VALUE
 
@@ -72,6 +73,7 @@ class OpenCVCamera : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
         itemPreviewGray = menu.add(getString(R.string.gray_preview))
         previewCanny = menu.add(getString(R.string.preview_canny))
         previewFeatures = menu.add(getString(R.string.preview_features))
+        previewBinary = menu.add(getString(R.string.preview_binary))
 
         return true
     }
@@ -88,6 +90,8 @@ class OpenCVCamera : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
             viewMode = VIEW_MODE_CANNY
         } else if (item === previewFeatures) {
             viewMode = VIEW_MODE_FEATURES
+        } else if (item === previewBinary) {
+            viewMode = VIEW_MODE_BINARY
         }
 
         return true
@@ -128,6 +132,26 @@ class OpenCVCamera : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
                 imageGray = inputFrame.gray()
                 FindFeatures(imageGray.nativeObjAddr, image.nativeObjAddr)
             }
+
+            VIEW_MODE_BINARY -> {
+
+                val blur1 = Mat()
+                val blur2 = Mat()
+                imageGray = inputFrame.gray()
+
+                Imgproc.cvtColor(ret_mat, image, Imgproc.COLOR_GRAY2RGBA, 4)
+
+                Imgproc.GaussianBlur(imageGray, blur1, Size(15.0, 15.0), 5.0)
+                Imgproc.GaussianBlur(imageGray, blur2, Size(21.0, 21.0), 5.0)
+
+                val gaussianDifference = Mat()
+                Core.absdiff(blur1, blur2, gaussianDifference)
+
+                Core.multiply(gaussianDifference, Scalar(100.00), gaussianDifference)
+                Imgproc.threshold(gaussianDifference, image, 50.0, 255.0, Imgproc.THRESH_BINARY_INV)
+
+                //Utils.matToBitmap(gaussianDifference, image)
+            }
         }
 
         return image
@@ -143,5 +167,6 @@ class OpenCVCamera : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
         private val VIEW_MODE_GRAY = 1
         private val VIEW_MODE_CANNY = 2
         private val VIEW_MODE_FEATURES = 5
+        private val VIEW_MODE_BINARY = 10
     }
 }
